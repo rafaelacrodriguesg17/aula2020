fc7340494edd8274cc232cf6cb5f8e50a9b4a276
RAFAELA CRISTINA RODRIGUES GONCALVES <rafaela.crgoncalves@senacsp.edu.br>
2020-01-23 10:57:38 -0300

    global

--- web/src/App.js
+++ web/src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import CompA from './componentes/CompA'
 import CompB from './componentes/Props';
 import CompC from './componentes/State';
+import Form from './componentes/formulario/form';
 
 function App() { // • Sempre tem que estar dentro de uma "div" se tiver mais de um componente.
   return (
     <div>
+      < Form />
       < CompA />
       < CompB nome = "Rafaela" sobrenome = "Gonçalves" />
       <CompC />
---
+++ web/src/componentes/formulario/form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './form.css';
+// import {Container} from './style'
+
+export default function Form(){
+    return(
+        <div className="div"> TESTE </div>
+    );
+}
