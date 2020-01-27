6b524cacea56957f46167ebe30844055784e351f
RAFAELA CRISTINA RODRIGUES GONCALVES <rafaela.crgoncalves@senacsp.edu.br>
2020-01-27 11:29:44 -0300

    terminar.2

--- web/src/meuSite/carousel/carousel.js
+++ web/src/meuSite/carousel/carousel.js
@@ -14,24 +14,25 @@ export default function Carousel(){
                 <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
                 <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
             </ol>
-            <div class="carousel-inner"/>
-                <div class="carousel-item active"/>
+            <div className="carousel"/>
+                <div class="imagem01"/>
                 <img src={Imagem01}></img>
-            <div class="carousel-caption d-none d-md-block">
+            <div className="imagem01">
                 <h5 className="subtitulo-h5"> 10 Picolés </h5>
                 <p className="paragrafo-p">Tudo por R$6,00. Venha aproveitar!</p>
             </div>
         </div>
-        <div class="carousel-item">
-            <img src={Imagem02}></img>
-            <div class="carousel-caption d-none d-md-block">
+        <div className="imagem02">
+            {/*<img src={Imagem02}></img>*/}
+            <div className="imagem02">
                 <h5 className="subtitulo-h5"> Sorvetes </h5>
                 <p className="paragrafo-p"> Dois sorvetes por R$15,00. Venha conferir! </p>
             </div>
         </div>
-        <div class="carousel-item">
-            <img src={Imagem03}></img>
-            <div class="carousel-caption d-none d-md-block">
+        
+        <div className="imagem03">
+            {/*<img src={Imagem03}></img>*/}
+            <div className="imagem03">
                 <h5 className="subtitulo-h5"> Quarta da alegria </h5>
                 <p className="paragrafo-p"> Toda quarta-feira uma promoção + entrega. Fique atento em nossas redes sociais! </p>
             </div>
@@ -47,7 +48,6 @@ export default function Carousel(){
                 <span class="sr-only">Próximo</span>
             </a>
         </div>
-        
     </div>
     )
 
